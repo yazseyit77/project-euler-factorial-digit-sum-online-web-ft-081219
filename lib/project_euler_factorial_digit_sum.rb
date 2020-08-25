767cee0ef0d37513c88eaf29f0f44f0a54a31a0b
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-25 00:03:54 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,17 @@
 # code your solution here
+
+def factorial(n)
+  if n < 3
+    n
+  else
+    n * factorial(n - 1)
+  end
+end
+
+def sum_of_digits(n)
+  n.to_s.chars.map{|k| k.to_i}.reduce(:+)
+end
+
+def factorial_digit_sum(n)
+  sum_of_digits(factorial(n))
+end
